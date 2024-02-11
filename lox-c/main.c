f79928b63dbd1846fd0b96e089612730bace9d08
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-02-11 08:06:24 +0100

    Add subtraction

--- lox-c/main.c
+++ lox-c/main.c
@@ -3,9 +3,15 @@
 
 int main(int argc, const char *argv[]) {
   initVM();
+
   Chunk chunk;
   initChunk(&chunk);
+  writeConstant(&chunk, 1250.0, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
+  writeChunk(&chunk, OP_RETURN, 123);
+
   interpret(&chunk);
+
   freeVM();
   freeChunk(&chunk);
 }
--- lox-c/vm.c
+++ lox-c/vm.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+#include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "value.h"
 #include "vm.h"
 
 VM vm;
 
-void initVM() {}
+static void resetStack() { vm.stackTop = vm.stack; }
+
+void initVM() { resetStack(); }
 
 void freeVM() {}
 
@@ -16,19 +20,39 @@ static InterpretResult run() {
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
-    case OP_RETURN: {
-      return INTERPRET_OK;
-    }
     case OP_CONSTANT: {
       Value constant = READ_CONSTANT();
-      printValue(constant);
-      printf("\n");
+      push(constant);
       break;
+    }
+    case OP_CONSTANT_LONG: {
+      // We read 3 bytes which encode the index of the constant.
+      int constant_index = ((int)READ_BYTE()) << 16;
+      constant_index |= ((int)READ_BYTE()) << 8;
+      constant_index |= READ_BYTE();
+
+      Value constant = vm.chunk->constants.values[constant_index];
+      push(constant);
+    }
+    case OP_NEGATE: {
+      push(-pop());
+    }
+    case OP_RETURN: {
+      printValue(pop());
+      printf("\n");
+      return INTERPRET_OK;
     }
     }
   }
@@ -41,4 +65,14 @@ InterpretResult interpret(Chunk *chunk) {
   vm.chunk = chunk;
   vm.ip = vm.chunk->code;
   return run();
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
