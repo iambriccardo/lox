51cc0e518e62a4be74efcab46f4634ac491cec30
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-03-03 17:29:59 +0100

    Remove unused function

--- lox-c/value.c
+++ lox-c/value.c
@@ -62,15 +62,6 @@ bool valuesEqual(Value a, Value b) {
   }
 }
 
-static uint32_t hashString(const char *key, int length) {
-  uint32_t hash = 2166136261u;
-  for (int i = 0; i < length; i++) {
-    hash ^= (uint8_t)key[i];
-    hash *= 16777619;
-  }
-  return hash;
-}
-
 uint32_t valueHash(Value value) {
   switch (value.type) {
   case VAL_BOOL:
