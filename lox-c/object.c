66dea63fa45e1f0918991d71fcf58c3c45a3fbbd
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-05-09 12:26:20 +0200

    Add basic closure structure

--- lox-c/object.c
+++ lox-c/object.c
@@ -20,6 +20,12 @@ static Obj *allocateObject(size_t size, ObjType type) {
   return object;
 }
 
+ObjClosure *newClosure(ObjFunction *function) {
+  ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+  closure->function = function;
+  return closure;
+}
+
 ObjFunction *newFunction() {
   ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
   function->arity = 0;
@@ -110,6 +116,9 @@ ObjString *referenceString(const char *start, int length) {
 
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
+  case OBJ_CLOSURE:
+    printFunction(AS_CLOSURE(value)->function);
+    break;
   case OBJ_FUNCTION:
     printFunction(AS_FUNCTION(value));
     break;
--- lox-c/object.h
+++ lox-c/object.h
@@ -7,10 +7,12 @@
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define AS_CLOSURE(value) ((ObjClosure *)AS_OBJ(value))
 #define AS_FUNCTION(value) ((ObjFunction *)AS_OBJ(value))
 #define AS_NATIVE(value) (((ObjNative *)AS_OBJ(value))->function)
 #define AS_STRING(value) ((ObjString *)AS_OBJ(value))
@@ -21,6 +23,7 @@
 #define IS_REF_STRING(objString) objString->start != NULL
 
 typedef enum {
+  OBJ_CLOSURE,
   OBJ_FUNCTION,
   OBJ_NATIVE,
   OBJ_STRING,
@@ -54,6 +57,12 @@ struct ObjString {
   char chars[];
 };
 
+struct ObjClosure {
+  Obj obj;
+  ObjFunction *function;
+};
+
+ObjClosure *newClosure(ObjFunction *function);
 ObjFunction *newFunction();
 ObjNative *newNative(NativeFn function);
 ObjString *takeString(char *chars, int length);
